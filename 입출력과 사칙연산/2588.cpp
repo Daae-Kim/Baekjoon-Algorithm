c0406719706c6526e069969f06c82e7c7f80e3a6
Daae-Kim <wlsemrdl1212@naver.com>
2021-03-30 00:13:29 +0900

    correct: 2588
    백준알고리즘 2588번 정답 소스코드

---
+++ 입출력과 사칙연산/2588.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A, B, C, D;
+    cin >> A;
+    cin >> B >> C >> D;
+
+    if (A < 100)
+        return false;
+
+    cout << B << C << D;
+}
